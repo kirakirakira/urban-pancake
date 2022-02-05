4621232d5a91d2c0a2f41dc3d9d11451f8114042
Kira Hartlage <kira.hartlage@gmail.com>
2022-02-05 14:23:56 -0500

    test for person that doesn'texist

--- UrbanPancake.LibraryTests/PersonRepositoryTest.cs
+++ UrbanPancake.LibraryTests/PersonRepositoryTest.cs
@@ -6,7 +6,7 @@ namespace UrbanPancake.Library
     public class PersonRepositoryTest
     {
         [Fact]
-        public void HasAPerson()
+        public void ShouldAddAPersonAndFindThem()
         {
             PersonRepository personRepository = new PersonRepository();
             Person expected = new Person
@@ -19,6 +19,22 @@ namespace UrbanPancake.Library
             personRepository.Add(expected);
 
             Assert.Equal(expected, personRepository.FindPersonWith("Kira", "Bubbles"));
+        }
+
+        [Fact]
+        public void ShouldReturnNullIfPersonIsNotFound()
+        {
+            PersonRepository personRepository = new PersonRepository();
+            Person expected = new Person
+            {
+                FirstName = "Kira",
+                LastName = "Bubbles",
+                PhoneNumber = "555-123-4567"
+            };
+
+            personRepository.Add(expected);
+
+            Assert.Null(personRepository.FindPersonWith("Ryan", "Bubbles"));
         }
     }
 }
