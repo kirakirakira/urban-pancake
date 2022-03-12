f47185efb4f6f2c56c7a4513a801520d6aa14957
Kira Hartlage <kira.hartlage@gmail.com>
2022-03-12 11:52:16 -0500

    remove duplicated code

--- UrbanPancake.Library/Menus/IMenuItem.cs
+++ UrbanPancake.Library/Menus/IMenuItem.cs
@@ -3,6 +3,5 @@ namespace UrbanPancake.Library
     public interface IMenuItem
     {
         string Choice { get; set; }
-        // int ExecuteChoice();
     }
 }
--- UrbanPancake.Library/Menus/Tool/ToolMenu.cs
+++ UrbanPancake.Library/Menus/Tool/ToolMenu.cs
@@ -17,39 +17,5 @@ namespace UrbanPancake.Library
         }
 
         public string Choice { get; set; } = "Tool Menu. Make a selection.";
-
-        private static int ShowMenu()
-        {
-            for (int i = 0; i < items.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {items[i].Choice}");
-            }
-
-            int userChoice = MenuMethods.GetNumber("Enter your choice: ");
-
-            Console.WriteLine("\n");
-            Console.WriteLine($"Your choice was {userChoice}");
-            Console.WriteLine("\n");
-
-            if (userChoice > items.Count)
-            {
-                Console.WriteLine("You failed to make a valid choice, try again!");
-                return (int)MenuFunctions.ContinueCurrentMenu;
-            }
-            else
-            {
-                return items[userChoice - 1].ExecuteChoice();
-            }
-        }
-
-        public int ExecuteChoice()
-        {
-            var keepGoing = (int)MenuFunctions.ContinueCurrentMenu;
-            while (keepGoing == (int)MenuFunctions.ContinueCurrentMenu)
-            {
-                keepGoing = ShowMenu();
-            }
-            return (int)MenuFunctions.ContinueMainMenu;
-        }
     }
 }
